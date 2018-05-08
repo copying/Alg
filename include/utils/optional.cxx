c218d64a9237b66d77dee798db5e64979979dc1e
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-05-08 17:03:50 +0200

    Add std::optional related utilities.

---
+++ include/utils/optional.cxx
@@ -0,0 +1,55 @@
+#ifndef ALG__UTILS__OPTIONAL__CXX
+#define ALG__UTILS__OPTIONAL__CXX
+
+#include <type_traits>
+#include <optional>
+
+// is_optional
+namespace alg::utils {
+    namespace {
+        template<typename O, typename = void>
+        struct is_optional_ : std::false_type {};
+
+        template<template <typename> typename O, typename T>
+        struct is_optional_<
+            O<T>,
+            std::enable_if_t<
+                std::is_same_v<
+                    O<T>,
+                    std::optional<T>
+                >
+            >
+        > : std::true_type {};
+    }
+    template <typename T>
+    using is_optional = is_optional_<std::decay_t<T>>;
+
+    template <typename T>
+    [[maybe_unused]]
+    constexpr auto const is_optional_v = is_optional<T>::value;
+}
+
+
+
+// make_optional
+namespace alg::utils {
+    namespace {
+        template<typename T>
+        using make_optional_ =
+            std::conditional<
+                utils::is_optional_v<T>,
+                T,
+                std::optional<T>
+            >;
+    }
+
+    template<typename T>
+    using make_optional = make_optional_<std::decay_t<T>>;
+
+    template<typename T>
+    using make_optional_t = typename make_optional<T>::type;
+}
+
+
+
+#endif // ALGUTILS__OPTIONAL__CXX
---
+++ tests/utils/test_optional.cpp
@@ -0,0 +1,72 @@
+#include <catch.hpp>
+
+
+#include "../../include/utils/optional.cxx"
+
+#include <optional>
+
+
+using namespace alg::utils;
+
+struct A {};
+
+
+
+
+TEST_CASE("is_optional checks if a type is of the type std::optional", "[is_optional]") {
+    SECTION("a few basic types") {
+        REQUIRE_FALSE(is_optional_v<void>);
+        REQUIRE_FALSE(is_optional_v<int>);
+        REQUIRE_FALSE(is_optional_v<float>);
+        REQUIRE_FALSE(is_optional_v<A>);
+    }
+
+    SECTION("basic optional types") {
+        REQUIRE(is_optional_v<std::optional<int>>);
+        REQUIRE(is_optional_v<std::optional<float>>);
+        REQUIRE(is_optional_v<std::optional<A>>);
+    }
+
+    SECTION("cv qualified types") {
+        REQUIRE_FALSE(is_optional_v<const int>);
+        REQUIRE_FALSE(is_optional_v<const float>);
+        REQUIRE_FALSE(is_optional_v<const A>);
+
+        REQUIRE_FALSE(is_optional_v<const int&>);
+        REQUIRE_FALSE(is_optional_v<const float&>);
+        REQUIRE_FALSE(is_optional_v<const A&>);
+
+        REQUIRE_FALSE(is_optional_v<volatile int>);
+        REQUIRE_FALSE(is_optional_v<volatile float>);
+        REQUIRE_FALSE(is_optional_v<volatile A>);
+
+        REQUIRE(is_optional_v<const std::optional<int>&>);
+        REQUIRE(is_optional_v<const std::optional<float>&>);
+        REQUIRE(is_optional_v<const std::optional<A>&>);
+    }
+}
+
+
+
+
+TEST_CASE("make_optional returns the optional type of the given type", "[make_optional]") {
+    SECTION("basic types") {
+        REQUIRE(std::is_same_v<make_optional_t<int>, std::optional<int>>);
+        REQUIRE(std::is_same_v<make_optional_t<float>, std::optional<float>>);
+        REQUIRE(std::is_same_v<make_optional_t<A>, std::optional<A>>);
+    }
+
+    SECTION("cv qualified types") {
+        REQUIRE(std::is_same_v<make_optional_t<const int>, std::optional<int>>);
+        REQUIRE(std::is_same_v<make_optional_t<const float>, std::optional<float>>);
+        REQUIRE(std::is_same_v<make_optional_t<const A>, std::optional<A>>);
+
+        REQUIRE(std::is_same_v<make_optional_t<const int&>, std::optional<int>>);
+        REQUIRE(std::is_same_v<make_optional_t<const float&>, std::optional<float>>);
+        REQUIRE(std::is_same_v<make_optional_t<const A&>, std::optional<A>>);
+
+        REQUIRE(std::is_same_v<make_optional_t<volatile int>, std::optional<int>>);
+        REQUIRE(std::is_same_v<make_optional_t<volatile float>, std::optional<float>>);
+        REQUIRE(std::is_same_v<make_optional_t<volatile A>, std::optional<A>>);
+    }
+}
